4ea121d27118941317ffb6cf83f397464fa71a10
GregorioFornetti <fornetti.greg@gmail.com>
2021-08-30 22:03:55 -0300

    menu principal
    - Prototipo menu principal
    - Renomeações

---
+++ Cenas/Menus/Main_menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+func _ready():
+	pass 
+
+
+func _on_Btn_play_pressed():
+	pass
+
+func _on_Btn_options_pressed():
+	pass
+
+func _on_Btn_credits_pressed():
+	pass
+
+func _on_Btn_exit_pressed():
+	get_tree().quit()
